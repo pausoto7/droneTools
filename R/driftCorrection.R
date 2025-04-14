e8429f5491ce3f512e05a59acf3c859b35ddfa0b
pausoto7 <paula.sooto@gmail.com>
2025-04-14 13:35:37 -0700

    made into function, more user friendly

--- R/driftCorrection.R
+++ R/driftCorrection.R
@@ -7,109 +7,127 @@ library(round)
 library(stringr)
 library(lubridate)
 
+# path to drift csv data that is laid out in | DateTime | Real Temp | Drone Temp | columns 
+# DateTime should be laid out as yyyy-mm-dd hh:mm:ss in csv for best result with processing
+drift_data_csv <- "data/exampleDriftData.csv"
 
-# prepare power equation data ----------------------------------------------------
+# path where rasters are located
+raster_filepath <- "C:/Users/sotop/Documents/Drone Projects/Cedardownstream_005_Feb2025/thermal_processed_kelvin/11-04-2025 1538-19"
 
-# upload csv file, change col into Posixct file type, and rename cols to reduce issues in code
-fieldData_raw <- read_csv("data/exampleDriftData.csv", show_col_types = FALSE)
+# path where you want corrected rasters to be output - will create a folder if it doesn't already exist
 
-# Overwrite column names
-colnames(fieldData_raw) <- c("DateTime", "RealTemp", "DroneTemp")
-
-# Convert DateTime
-fieldData_raw <- fieldData_raw %>%
-  mutate(DateTime = ymd_hms(DateTime, tz = "UTC"))
-
-driftStartEnd <- fieldData_raw$DateTime
-
-# max time minus 1 second because log(0) gives an error
-minTime <- min(driftStartEnd) - seconds(1)
-
-
-# add temp diff
-fieldData <- fieldData_raw %>%
-  mutate(departures = DroneTemp-RealTemp) %>%
-  mutate(timePassed =  as.numeric(DateTime - minTime), .before = DateTime)
+output_location <- "C:/Users/sotop/Documents/Drone Projects/Cedardownstream_005_Feb2025/Thermal_drift_corrected"
 
 
 
-# Log of first and second timePassed
-log_t1 <- log(fieldData$timePassed[1])
-log_t2 <- log(fieldData$timePassed[2])
-
-# Log of first and second departures
-log_y1 <- log(fieldData$departures[1])
-log_y2 <- log(fieldData$departures[2])
-
-# Calculate b
-b <- (log_y2 - log_y1) / (log_t2 - log_t1)
+# CAN'T DO WINTER TEMPS YET - ADD FEATURE
 
 
-# check that all time values are positive
-if (any(fieldData$timePassed <= 0)) stop("Non-positive time values found!")
 
-# Use the first observation
-y1 <- fieldData$departures[1]
-t1 <- fieldData$timePassed[1]
 
-# Calculate k
-k <- y1 / (t1^b)
-
-# prepare imagery 
-
-# convert files from kelvin to celcius
-
-tiff_file_list <- list.files("C:/Users/sotop/Documents/Drone Projects/Cedardownstream_005_Feb2025/thermal_processed_kelvin/11-04-2025 1538-19",
-                             full.names = TRUE)
-
-output_location <- "C:/Users/sotop/Documents/Drone Projects/Cedardownstream_005_Feb2025/Thermal_drift_corrected/"
-
-for(files in 1:length(tiff_file_list)){
+correctTemperatureDrift <- function(drift_data_csv, raster_filepath, output_location ){
+  
+  # prepare  equation data ----------------------------------------------------
+  
+  # upload csv file, change col into Posixct file type, and rename cols to reduce issues in code
+  fieldData_raw <- read_csv(drift_data_csv, show_col_types = FALSE)
+  
+  # Overwrite column names
+  colnames(fieldData_raw) <- c("DateTime", "RealTemp", "DroneTemp")
+  
+  # Convert DateTime
+  fieldData_raw <- fieldData_raw %>%
+    mutate(DateTime = ymd_hms(DateTime, tz = "UTC"))
+  
+  # start time minus 1 second because log(0 seconds) is mathematically undefined
+  minTime <- min(fieldData_raw$DateTime) - seconds(1)
+  
+  # add temp diffs and time passed in seconds
+  fieldData <- fieldData_raw %>%
+    mutate(departures = DroneTemp-RealTemp) %>%
+    mutate(timePassed =  as.numeric(DateTime - minTime), .before = DateTime)
+  
+  # Log of first and second timePassed
+  log_t1 <- log(fieldData$timePassed[1])
+  log_t2 <- log(fieldData$timePassed[2])
+  
+  # Log of first and second departures
+  log_y1 <- log(fieldData$departures[1])
+  log_y2 <- log(fieldData$departures[2])
+  
+  # Calculate b
+  b <- (log_y2 - log_y1) / (log_t2 - log_t1)
   
   
-  # get drift value -----------------------------------------
-  file <- tiff_file_list[files]
+  # check that all time values are positive
+  if (any(fieldData$timePassed <= 0)) stop("Non-positive time values found!")
   
-  file.path <- dirname(file)
+  # Use the first observation
+  y1 <- fieldData$departures[1]
+  t1 <- fieldData$timePassed[1]
   
-  only_filename <- basename(file)
-  new_filename <- paste0(sub(".TIFF$", "_corrected.tiff", only_filename, ignore.case = TRUE))
-  output_filename <- paste0(output_location, new_filename)
+  # Calculate k
+  k <- y1 / (t1^b)
+  
+  # modify imagery values  ----------------------------------------------------
+  
 
+  tiff_file_list <- list.files(raster_filepath,
+                               full.names = TRUE)
   
-  tiff_metadata <- exifr::read_exif(file)
-  
-  raster_datetime <- ymd_hms(tiff_metadata$DateTimeOriginal)
-  
-  t <- as.numeric(raster_datetime - minTime) 
-  
-  drift_value <- k*t^b
-  
-  
-  # convert tiff --------------------------------------------
-  
-  tiff <- raster::raster(file)
-  
-  thermal_values_k <- values(tiff)
-  
-  thermal_values_c <- (thermal_values_k/100) - 273.15
-  
-  corrected_values <- thermal_values_c - drift_value
-  
-  corrected_raster <- tiff
-  
-  raster::values(corrected_raster) <- corrected_values
-  
-  if (!dir.exists(output_location)) {
-    dir.create(output_location, recursive = TRUE)
-    sprintf("Create output folder located here: %s", output_location)
+  # loop through each raster
+  for(files in 1:length(tiff_file_list)){
+    
+    # set up file input and output names
+    file <- tiff_file_list[files]
+    file.path <- dirname(file)
+    only_filename <- basename(file)
+    new_filename <- paste0(sub(".TIFF$", "_corrected.tiff", only_filename, ignore.case = TRUE))
+    output_filename <- paste0(output_location, "/", new_filename)
+    
+    # get variables needed for calculations------------------
+    tiff_metadata <- exifr::read_exif(file)
+    
+    raster_datetime <- ymd_hms(tiff_metadata$DateTimeOriginal)
+    
+    # calculate time passed
+    t <- as.numeric(raster_datetime - minTime) 
+    
+    # calculate drift value for this raster
+    drift_value <- k*t^b
+
+    # convert tiff --------------------------------------------
+    
+    tiff <- raster::raster(file)
+    
+    thermal_values_k <- values(tiff)
+    
+    # convert from kelvin to celcius as ThermoConverter outputs in Kelvin
+    thermal_values_c <- (thermal_values_k/100) - 273.15
+    
+    corrected_values <- thermal_values_c - drift_value
+    
+    corrected_raster <- tiff
+    
+    raster::values(corrected_raster) <- corrected_values
+    
+    # if directory doesn't exist create it
+    if (!dir.exists(output_location)) {
+      dir.create(output_location, recursive = TRUE)
+      sprintf("Create output folder located here: %s", output_location)
+    }
+    
+    # write raster with corrected values in Celsius
+    writeRaster(corrected_raster, filename = output_filename, format = "GTiff", overwrite = TRUE)
+    
   }
   
-  writeRaster(corrected_raster, filename = output_filename, format = "GTiff", overwrite = TRUE)
+  
+  
   
 }
 
-# can't do exponents with negative values ... could pose a problem in artic conditions?
+
 
 
 
